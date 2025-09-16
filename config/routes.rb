4d2d0296bc9c87d7aeac43f0882a76555e52cd9d
MuhammadRaza29 <raza.aslam29@gmail.com>
2025-09-17 00:33:39 +0500

    Fix reset plan config issue

--- config/routes.rb
+++ config/routes.rb
@@ -560,9 +560,7 @@ Rails.application.routes.draw do
       resources :platform_apps, only: [:index, :new, :create, :show, :edit, :update, :destroy]
       resource :instance_status, only: [:show]
 
-      resource :settings, only: [:show] do
-        get :refresh, on: :collection
-      end
+      resource :settings, only: [:show]
 
       # resources that doesn't appear in primary navigation in super admin
       resources :account_users, only: [:new, :create, :show, :destroy]
